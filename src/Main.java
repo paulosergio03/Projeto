bde45f599fbd382d29ed59805fe722245cccc361
Paulo Sérgio <paulosergioadm9@gmail.com>
2025-07-23 00:10:09 -0300

    Alteração

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
             int y = 32;
+            double x = 10.35784;
             System.out.println(y);
+            System.out.println("Bom dia!");
+            //Formato para duas casas decimais
+            System.out.printf("%.2f%n", x);
+            //Formato para quatro casas decimais
+            Locale.setDefault(Locale.US);
+            System.out.printf("%.4f%n", x);
+
+
         }
     }
